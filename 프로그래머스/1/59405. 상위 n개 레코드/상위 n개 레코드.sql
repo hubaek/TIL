5d68e32860aa28a3171764594b3b1457cd9647e1
hubaek <91240642+hubaek@users.noreply.github.com>
2024-09-12 09:40:52 +0900

    [level 1] Title: 상위 n개 레코드, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+# 서브쿼리를 where에 하는게 아니라 from으로 해서 결과가 나온 값을 where절로 조건을 해야함
+# 서브쿼리에서 rank값만 얻을려고 rank를 넣는게 아닌 name을 조회하기 위해선 name필드도 넣어야 함 
+SELECT name
+from (
+    select name, rank() over (order by datetime) AS ranking
+     from animal_ins
+     ) as ranked_animals
+     where ranking = 1
+;
