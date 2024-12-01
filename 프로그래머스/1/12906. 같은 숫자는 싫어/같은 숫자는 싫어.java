81a10a6ff2e1499f71df640eb37537b9c2317461
hubaek <91240642+hubaek@users.noreply.github.com>
2024-12-01 19:39:32 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 52.15 ms, Memory: 114 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        
+        Stack<Integer> answer = new Stack<>();
+        
+        for (int num : arr) {
+            if(answer.isEmpty() || num != answer.peek()) {
+                answer.push(num);
+            }
+        }
+        
+
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
