01ca1454dbb717ef0e241856911fde18eea819ba
hubaek <91240642+hubaek@users.noreply.github.com>
2025-02-18 18:55:33 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.25 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder(my_string);
+        for(int i = 0; i < overwrite_string.length(); i++) {
+            sb.setCharAt(i+s, overwrite_string.charAt(i));
+        }
+        
+        answer = sb.toString();
+        
+        return answer;
+    }
+}
