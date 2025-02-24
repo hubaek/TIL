49366638ea9dd4f3c3a4e9a504c831559b7238be
hubaek <91240642+hubaek@users.noreply.github.com>
2025-02-24 23:49:07 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.87 ms, Memory: 85.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        String ab = String.valueOf(a) + String.valueOf(b);
+        String ba = String.valueOf(b) + String.valueOf(a);
+        answer = Math.max(Integer.parseInt(ab), Integer.parseInt(ba));
+        return answer;
+    }
+}
