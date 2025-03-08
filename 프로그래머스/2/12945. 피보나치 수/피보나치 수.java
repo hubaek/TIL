1f39ba59cf85d75ad147c40dbd14d1a69c15b259
hubaek <91240642+hubaek@users.noreply.github.com>
2025-03-08 11:27:22 +0900

    [level 2] Title: 피보나치 수, Time: 4.95 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n) {
+        
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        
+        int a = 0;
+        int b = 1;
+        int sum;
+        
+        for (int i =2; i <= n; i++) {
+            sum = (a + b) % 1234567;
+            a = b  % 1234567;
+            b = sum  % 1234567;
+        }
+        
+        // answer = b;
+        
+        
+        return b;
+    }
+}
